afa749a3a032cb16ef6d46d7e52361838b2d9e44
AnastasiaV <Anubis.27007@mail.ru>
2022-05-02 23:43:28 +0300

    Commit 0.11

---
+++ Notes.WebApi/ConfigureSwaggerOptions.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Notes.WebApi
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)=>
+            _provider = provider;
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach(var description in _provider.ApiVersionDescriptions)
+            {
+                var apiVersion = description.ApiVersion.ToString();
+                options.SwaggerDoc(description.GroupName,
+                    new OpenApiInfo
+                    {
+                        Version = apiVersion,
+                        Title = $"Notes API {apiVersion}",
+                        Description = "A simple example ASP NET Core Web API. Professional way",
+                        /*Contact = new OpenApiContact
+                        {
+                            Name = " Platinum Chat",
+                            Email = String.Empty,
+                            Url = new Uri("https://t.me/platinum_chat"),
+
+                        },
+                        License = new OpenApiLicense
+                        {
+                            Name = "Platinum Telegram Channel",
+                            Url = new Uri("https://t.me/platinum_tech_talks")
+                        }*/
+                    });
+
+                options.CustomOperationIds(apiDescription => apiDescription.TryGetMethodInfo
+                (out MethodInfo methodInfo)
+                ? methodInfo.Name
+                : null);
+            }
+        }
+
+    }
+}
--- Notes.WebApi/Startup.cs
+++ Notes.WebApi/Startup.cs
@@ -10,6 +10,9 @@ using Notes.Persistence;
 using Notes.WebApi.Middleware;
 using System;
 using System.IO;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 
 namespace Notes.WebApi
 {
@@ -41,18 +44,17 @@ namespace Notes.WebApi
                     policy.AllowAnyOrigin();
                 });
             });
+            services.AddVersionedApiExplorer(options =>
+            options.GroupNameFormat = "'v'VVV" );
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>,
+                ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
 
-            services.AddSwaggerGen(config =>
-            {
-                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name }.xml";
-                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                config.IncludeXmlComments(xmlPath);
-            });
-            
-
+            services.AddApiVersioning();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            IApiVersionDescriptionProvider provider)
         {
             if(env.IsDevelopment())
             {
@@ -62,14 +64,22 @@ namespace Notes.WebApi
             app.UseSwagger();
             app.UseSwaggerUI(config =>
             {
-                config.RoutePrefix = string.Empty;
-                config.SwaggerEndpoint("swagger/v1/swagger.json", "Notes API");
+                foreach(var description in provider.ApiVersionDescriptions)
+                {
+                    config.SwaggerEndpoint(
+                        $"/swagger/{description.GroupName}/swagger.json",
+                        description.GroupName.ToUpperInvariant());
+                    config.RoutePrefix = string.Empty;
+                }
+                //config.RoutePrefix = string.Empty;
+                //config.SwaggerEndpoint("swagger/v1/swagger.json", "Notes API");
             });
            
             app.UseCustomExceptionHandler();
             app.UseRouting();
             app.UseHttpsRedirection();
             app.UseCors("AllowAll");
+            app.UseApiVersioning();
 
             app.UseEndpoints(endpoints =>
             {
